6871c7df014a953e5ebf90e72c1174870ada4486
LeoBock18 <leonardo.bocchieri@mail.polimi.it>
2024-05-31 20:05:59 +0200

    Implementing Jacobi loop

--- jacobi.cpp
+++ jacobi.cpp
@@ -2,9 +2,9 @@
 #include<functional>
 #include<vector>
 
-using Matrix = apsc::LinearAlgebra::Matrix<jacobi::Real,ORDERING::ROWMAJOR>;
+using Matrix = LinearAlgebra::MyMat0<Real, ROWMAJOR>;
 
-Matrix jacobi::solve(int n, std::function< Real (Real_vec) > f)
+Matrix jacobi::solve(int n, std::function< Real (Real_vec) > f, Real tol, std::size_t n_max)
 {
     int rank,size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -24,8 +24,9 @@ Matrix jacobi::solve(int n, std::function< Real (Real_vec) > f)
         start_idx += recv_counts[i];
     }
     
-    // Construct local matrices
+    // Construct local matrices and rhs
     Matrix local_mat(recv_counts[rank], n, 0);
+    Matrix local_f(recv_counts[rank],n);
 
     // Construct domain nodes (1D)
     std::vector<Real> x(n);
@@ -34,26 +35,95 @@ Matrix jacobi::solve(int n, std::function< Real (Real_vec) > f)
         x[i] = h*i;
     }
 
+    // Force matrix (put it in other loop if extra of dirichlet)
+    for(std::size_t i = 0; i < recv_counts[rank]; ++i)
+    {
+        for(std::size_t j = 0; j< n; ++j)
+        {
+            std::size_t curr_row = displ[rank] + i;
+            local_f(i,j) = f({x[curr_row],x[j]});
+        }
+    }
+
+/*
     // Boundary conditions along y
     for(std::size_t i = 0; i < recv_counts[rank]; ++i)
     {
         std::size_t curr_row = displ[rank] + i;
-        local_mat[i,0] = f({x[curr_row],x[0]});
-        local_mat[i,n-1] = f({x[curr_row],x[n-1]});
+        local_mat(i,0) = g_dir({x[curr_row],x[0]});
+        local_mat(i,n-1) = g_dir({x[curr_row],x[n-1]});
     }
 
-    // Boundary Conditions along x
+    // Boundary Conditions along x (only first and last rank)
     if(rank == 0 or rank == size)
     {
         std::size_t boundary_row = (rank == 0) ? 0 : local_rows-1;
         for(std:size_t j = 1; j < n-1; ++j)
         {
-            local_mat[boundary_row,j] = f({x[boundary_row],x[j]});
+            local_mat(boundary_row,j) = g_dir({x[boundary_row],x[j]});
         }
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+*/
+    
+    std::size_t n_iter{0};
+    bool all_stop = false;
+    Matrix new_mat(recv_counts[rank], n, 0);
+    while( !all_stop and n_iter < n_max )
+    {
+        // Adjacent rows passing
+        // Initialization
+        std::vector<Real> upper_row(n,0);
+        std::vector<Real> lower_row(n,0);
 
-    MPI_Barrier
+        // Sending lower row
+        if(rank != size-1)
+        {
+            MPI_Send(local_mat.row(recv_counts[rank]-1).data(), n, MPI_DOUBLE, rank+1, rank, MPI_COMM_WORLD);
+        }
+        // Receiving lower row (from rank pov, it is upper row)
+        if(rank != 0)
+        {
+            MPI_Recv(upper_row.data(), n, MPI_DOUBLE, rank-1, rank-1, MPI_COMM_WORLD, MPI_STATUS_IGNORED);
+        }
 
+        // Sending upper row
+        if(rank != 0)
+        {
+            MPI_Send(local_mat.row(0).data(), n, MPI_DOUBLE, rank-1, rank, MPI_COMM_WORLD);
+        }
+        // Receiving upper row (from rank pov, it is lower row)
+        if(rank != size-1)
+        {
+            MPI_Recv(lower_row.data(), n, MPI_DOUBLE, rank+1, rank+1, MPI_COMM_WORLD, MPI_STATUS_IGNORED);
+        }
+
+        // Jacobi iteration (common to all ranks)
+        for(std::size_t i = 1; i < recv_counts[rank]-1; ++i)
+        {
+            for(std::size_t j = 1; j < n-1; ++j)
+            {
+                new_mat(i,j) += 0.25 * (local_mat(i-1,j) + local_mat(i+1,j) + local_mat(i,j-1) + local_mat(i,j+1) + h*h*local_f(i,j));
+            }
+        }
+        // Jacobi iteration (adding upper row)
+        if(rank != 0)
+        {
+            for(std::size_t j = 1; j < n-1; ++j)
+            {
+                new_mat(0,j) += 0.25 * upper_row[j];
+            }
+        }
+        // Jacobi iteration (adding lower row)
+        if(rank != size-1)
+        {
+            for(std::size_t j = 1; j < n-1; ++j)
+            {
+                new_mat(recv_counts[rank]-1,j) += 0.25 * lower_row[j];
+            }
+        }
+
+    }
 
 
 
--- jacobi.hpp
+++ jacobi.hpp
@@ -2,15 +2,16 @@
 #define PARALLEL_JACOBI_METHOD_HPP
 
 #include<iostream>
-#include"Matrix.hpp"
-//#include<mpi.h>
+#include"MyMat0.hpp"
+#include<mpi.h>
 
 namespace jacobi
 {
 
 using Real = double;
+using Real_vec = std::array<Real,2>;
 
-apsc::LinearAlgebra::Matrix<Real,ORDERING::ROWMAJOR> solve();
+LinearAlgebra::MyMat0<Real, ROWMAJOR> solve(int, std::function< Real (Real_vec) >, Real, std::size_t);
         
 
 } // namespace jacobi
